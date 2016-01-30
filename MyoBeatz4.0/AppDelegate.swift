d3f369fedfdadd59ca41b3ae4d5d6db590367fee
Julien Homble <jhomble@juliens-macbook-pro.local>
2016-01-30 01:34:18 -0500

    Initial Commit

---
+++ MyoBeatz4.0/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MyoBeatz4.0
+//
+//  Created by Julien Homble on 1/30/16.
+//  Copyright © 2016 Julien Homble. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MyoBeatz4.0/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MyoBeatz4.0
+//
+//  Created by Julien Homble on 1/30/16.
+//  Copyright © 2016 Julien Homble. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
